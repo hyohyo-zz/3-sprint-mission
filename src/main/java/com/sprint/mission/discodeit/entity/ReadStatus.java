4ef5d226bf34eba9c89f1376ef5600bdbe55b21b
hyohyo-zz <akbkck8101@gmail.com>
2025-04-24 14:27:19 +0900

    feat: ReadStatus 클래스 추가

---
+++ src/main/java/com/sprint/mission/discodeit/entity/ReadStatus.java
@@ -0,0 +1,33 @@
+package com.sprint.mission.discodeit.entity;
+
+import lombok.Getter;
+
+import java.time.Instant;
+import java.util.UUID;
+
+/*사용자가 채널 별 마지막으로 메시지를 읽은 시간을 표현 하는 도메인
+* 사용자별 각 채널에 읽지 않은 메시지 확인하기
+* 메시지를 읽음 -> updatedAt 시간이 현재 시간으로*/
+@Getter
+public class ReadStatus {
+    private UUID id;
+    private UUID userId;
+    private UUID channelId;
+    private Instant lastReadTime;
+
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    public ReadStatus(UUID userId, UUID channelId, Instant createdAt, Instant updatedAt) {
+        this.id = UUID.randomUUID();
+        this.userId = userId;
+        this.channelId = channelId;
+        this.createdAt = Instant.now();
+        this.updatedAt = createdAt;
+    }
+
+    public void updateLastReadTime(Instant newReadTime) {
+        this.lastReadTime = newReadTime;
+        this.updatedAt = Instant.now();
+    }
+}
