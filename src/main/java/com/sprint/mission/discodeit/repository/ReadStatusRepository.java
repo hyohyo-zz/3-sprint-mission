7fa5780f1c071e212eb73d16ae544f747af0756f
hyohyo-zz <akbkck8101@gmail.com>
2025-04-24 16:26:56 +0900

    feat: ReadStatusRepository 인터페이스 선언

--- src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
@@ -1,4 +1,27 @@
 package com.sprint.mission.discodeit.repository;
 
+import com.sprint.mission.discodeit.entity.ReadStatus;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface ReadStatusRepository {
+    public void create(ReadStatus readStatus);
+
+    public ReadStatus read(UUID id);
+
+    public List<ReadStatus> readAll();
+
+    public ReadStatus update(ReadStatus readStatus);
+
+    public ReadStatus delete(UUID id);
+
+    public void deleteByUserId(UUID userId);
+
+    public List<ReadStatus> readByUserId(UUID userId);
+
+    public void deleteByChannelId(UUID channelId);
+
+    public List<ReadStatus> readByChannelId(UUID channelId);
+
 }
