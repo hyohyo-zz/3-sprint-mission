9057946861f7776aff25fb6b174ad727ad072d10
hyohyo-zz <akbkck8101@gmail.com>
2025-04-24 16:40:54 +0900

    feat: BinaryContentRepository 인터페이스 선언

--- src/main/java/com/sprint/mission/discodeit/repository/BinaryContentRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/BinaryContentRepository.java
@@ -1,4 +1,23 @@
 package com.sprint.mission.discodeit.repository;
 
+import com.sprint.mission.discodeit.entity.BinaryContent;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface BinaryContentRepository {
+    public void save(BinaryContent binaryContent);
+
+    public BinaryContent read(UUID id);
+
+    public BinaryContent delete(UUID id);
+
+    //특정 유저의 (프로필 이미지 등) 바이너리 조회
+    public List<BinaryContent> readByUserId(UUID userId);
+
+    public List<BinaryContent> readByMessageId(UUID messageId);
+
+    public void deleteByMessageId(UUID messageId);
+
+
 }
