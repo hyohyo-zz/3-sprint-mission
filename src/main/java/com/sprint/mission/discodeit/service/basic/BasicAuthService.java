89d6e3936efb396e080aec886302000c1c8b0ad2
hyohyo-zz <akbkck8101@gmail.com>
2025-05-14 10:42:38 +0900

    feat: AuthController 구현

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
@@ -5,7 +5,9 @@ import com.sprint.mission.discodeit.dto.request.LoginRequest;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.service.AuthService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BasicAuthService implements AuthService {
     private final UserRepository userRepository;
 
