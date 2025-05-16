6f756d64e608117aede00a67c66879aa8453f29b
hyohyo-zz <akbkck8101@gmail.com>
2025-05-16 17:56:04 +0900

    docs: dto @Schema

--- src/main/java/com/sprint/mission/discodeit/dto/request/create/UserCreateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/create/UserCreateRequest.java
@@ -1,8 +1,16 @@
 package com.sprint.mission.discodeit.dto.request.create;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record UserCreateRequest(
+
+    @Schema(description = "유저 이름", example = "현아공주")
     String name,
+
+    @Schema(description = "유저 이메일", example = "hyuna@example.com")
     String email,
+
+    @Schema(description = "유저 비밀번호", example = "securePassword123!")
     String password
 ) {
 
--- src/main/java/com/sprint/mission/discodeit/dto/request/update/ChannelUpdateRequest_public.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/update/ChannelUpdateRequest_public.java
@@ -1,9 +1,13 @@
 package com.sprint.mission.discodeit.dto.request.update;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
 public record ChannelUpdateRequest_public(
+    @Schema(description = "새로운 채널 이름", example = "업데이트된 채널명")
     String newChannelName,
+
+    @Schema(description = "새로 설정할 카테고리 목록", example = "[\"공지\", \"자유\"]")
     List<String> newCategories
 ) {
 
