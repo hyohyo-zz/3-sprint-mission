13dfa6c5f925f6111c06e1587b3c9d5b5d329ada
hyohyo-zz <akbkck8101@gmail.com>
2025-05-27 15:19:21 +0900

    feat: ddl파일 추가, yaml db설정값 추가, 의존성 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,96 @@
+CREATE SCHEMA IF NOT EXISTS discodeit;
+
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS channels CASCADE;
+DROP TABLE IF EXISTS messages CASCADE;
+DROP TABLE IF EXISTS binary_contents CASCADE;
+DROP TABLE IF EXISTS user_statuses CASCADE;
+DROP TABLE IF EXISTS message_attachments CASCADE;
+
+-- binary_contents
+create table if not exists binary_contents
+(
+    id           uuid primary key,
+    created_at   timestamptz  not null,
+    file_name    varchar(255) not null,
+    size         bigint       not null,
+    content_type varchar(100) not null,
+    bytes        bytea        not null
+);
+
+create table if not exists users
+(
+    id         uuid primary key,
+    created_at timestamptz  not null,
+    updated_at timestamptz,
+    username   varchar(50)  not null unique,
+    email      varchar(100) not null unique,
+    password   varchar(60),
+    profile_id uuid,
+
+    CONSTRAINT fk_users_profile FOREIGN KEY (profile_id) REFERENCES binary_contents (id) ON DELETE SET NULL
+
+);
+
+create table if not exists channels
+(
+    id          uuid primary key,
+    created_at  timestamptz not null,
+    updated_at  timestamptz,
+    name        varchar(100),
+    description varchar(500),
+    type        varchar(10) not null
+);
+
+-- messages
+create table if not exists messages
+(
+    id         uuid primary key,
+    created_at timestamptz not null,
+    updated_at timestamptz,
+    content    text,
+    channel_id uuid        not null,
+    author_id  uuid,
+
+    CONSTRAINT fk_messages_channel FOREIGN KEY (channel_id) REFERENCES channels (id) ON DELETE CASCADE,
+    CONSTRAINT fk_messages_author FOREIGN KEY (author_id) REFERENCES users (id) ON DELETE SET NULL
+
+);
+
+-- user_statuses
+create table if not exists user_statuses
+(
+    id             uuid primary key,
+    created_at     timestamptz not null,
+    updated_at     timestamptz,
+    user_id        uuid        not null,
+    last_active_at timestamptz not null,
+
+    CONSTRAINT fk_user_statuses_userId FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+-- read_statuses
+create table if not exists read_statuses
+(
+    id           uuid primary key,
+    created_at   timestamptz not null,
+    updated_at   timestamptz,
+    user_id      uuid,
+    channel_id   uuid,
+    last_read_at timestamptz not null,
+
+    CONSTRAINT fk_read_statuses_userId FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    CONSTRAINT fk_read_statuses_channelId FOREIGN KEY (channel_id) REFERENCES channels (id) ON DELETE CASCADE,
+    CONSTRAINT uq_read_statuses_user_channel UNIQUE (user_id, channel_id)
+);
+
+-- message_attachments
+create table if not exists message_attachments
+(
+    message_id    uuid,
+    attachment_id uuid,
+
+    CONSTRAINT fk_message_attachments_messageId FOREIGN KEY (message_id) REFERENCES messages (id) ON DELETE CASCADE,
+    CONSTRAINT fk_message_attachments_attachmentId FOREIGN KEY (attachment_id) REFERENCES binary_contents (id) ON DELETE CASCADE
+);
+
