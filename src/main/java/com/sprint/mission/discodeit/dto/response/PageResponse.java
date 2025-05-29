ab45ce1eec30252ee690a056f46962e020fafe33
hyohyo-zz <akbkck8101@gmail.com>
2025-05-29 19:13:41 +0900

    feat: Slice, Page 로 dto 생성하는 mapper구현

---
+++ src/main/java/com/sprint/mission/discodeit/dto/response/PageResponse.java
@@ -0,0 +1,20 @@
+package com.sprint.mission.discodeit.dto.response;
+
+import java.util.List;
+
+/*
+* - `content`: 실제 데이터입니다.
+- `number`: 페이지 번호입니다.
+- `size`: 페이지의 크기입니다.
+- `totalElements`: T 데이터의 총 갯수를 의미하며, null일 수 있습니다.
+* */
+
+public record PageResponse<T>(
+    List<T> content,
+    int number,
+    int size,
+    boolean hasNext,
+    Long totalElements
+) {
+
+}
