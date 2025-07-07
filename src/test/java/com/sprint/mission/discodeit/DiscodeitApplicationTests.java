6c6a57313fadcbbacc58dba4df61915650cfc45a
hyohyo-zz <akbkck8101@gmail.com>
2025-07-07 11:57:34 +0900

    chore: 의존성 ë수정 test

--- src/test/java/com/sprint/mission/discodeit/DiscodeitApplicationTests.java
+++ src/test/java/com/sprint/mission/discodeit/DiscodeitApplicationTests.java
@@ -3,7 +3,6 @@ package com.sprint.mission.discodeit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DiscodeitApplicationTests {
 
     @Test
