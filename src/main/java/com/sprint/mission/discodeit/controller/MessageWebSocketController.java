cd04a0ff80959d21674ae45eb2aeb8452ee04cec
hyohyo-zz <akbkck8101@gmail.com>
2025-09-03 14:21:58 +0900

    feat: 메시지 송신 컨트롤러 구현

---
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageWebSocketController.java
@@ -0,0 +1,29 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.dto.request.MessageCreateRequest;
+import com.sprint.mission.discodeit.service.MessageService;
+import java.util.Collections;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class MessageWebSocketController {
+
+    private final MessageService messageService;
+
+    /**
+     *  단순 텍스트 메시지 전송(STOMP)
+     *  클라이언트 -> /pub/messages 로 발행
+     */
+    @MessageMapping("messages")
+    public void sendMessage(MessageCreateRequest request) {
+        if (request.content() == null || request.content().isBlank()) {
+            return;
+        }
+
+        messageService.create(request, Collections.emptyList());
+    }
+
+}
