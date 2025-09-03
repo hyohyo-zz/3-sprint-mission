2aac2b367a78266407055907458b533f15a7abec
hyohyo-zz <akbkck8101@gmail.com>
2025-09-03 14:15:15 +0900

    add: 웹소켓 의존성 추가, config 추가

---
+++ src/main/java/com/sprint/mission/discodeit/config/WebSocketConfig.java
@@ -0,0 +1,28 @@
+package com.sprint.mission.discodeit.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    /**
+     * 메시지 브로커 설정
+     * - SimpleBroker: /sub (구독 prefix)
+     * - Application Destination Prefix: /pub (발행 prefix)
+     */
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/sub");
+        config.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+    }
+}
